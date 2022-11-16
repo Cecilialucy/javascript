3daa52a991d4afeec208dd6ca173e86beae0f6c8
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-11-16 14:51:39 -0300

    exercício

---
+++ CommonJS/modules/mod1.js
@@ -0,0 +1,18 @@
+// module.exports.teste = "olá mundo"
+// module.exports = () => {
+//     console.log("função em module.exports")
+//     return "função em module.exports"
+// }
+// console.log("module.exports")
+// console.log(module.exports)
+// exports = {"prop": "não será exportado"}
+// console.log("exports")
+// console.log(exports)
+// console.log(module.exports === exports)
+
+// module.exports é o {} exportado
+
+exports.fn = () => {
+    console.log("função em module.exports")
+    return "-- função em module.exports --"
+}
---
+++ CommonJS/modules/mod_esm2.mjs
@@ -0,0 +1,4 @@
+export const teste = "teste nova exportada"
+
+
+// export sem colisão de nomes
